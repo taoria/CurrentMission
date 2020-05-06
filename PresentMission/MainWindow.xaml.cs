7b73017dc680e34668359a4d979891bd3cf27064
taoria <445625470@qq.com>
2020-05-06 21:33:44 +0800

    finish basic function

--- PresentMission/MainWindow.xaml.cs
+++ PresentMission/MainWindow.xaml.cs
@@ -20,6 +20,7 @@ namespace PresentMission
     /// </summary>
     public partial class MainWindow : Window
     {
+       
         public MainWindow()
         {
             InitializeComponent();
@@ -30,6 +31,80 @@ namespace PresentMission
 
             // Begin dragging the window
             this.DragMove();
+        }
+        private bool editing =false;
+        private bool working = false;
+        private void DoWork()
+        {
+            working = true;
+            commitWork.Visibility = Visibility.Visible;
+        }
+        private void FinishWork()
+        {
+            working = false;
+            textBoxWhenEdit.Text = "Mission to be set";
+            labelShow.Content = "Mission to be set";
+            commitWork.Visibility = Visibility.Hidden;
+
+        }
+        private void Toggle()
+        {
+            editing = !editing;
+            viewBoxLabel.Visibility = editing?Visibility.Hidden:Visibility.Visible;
+            viewBoxTextBox.Visibility = editing ? Visibility.Visible : Visibility.Hidden;
+            if (editing)
+            {
+                textBoxWhenEdit.Text = labelShow.Content.ToString();
+            }
+            else
+            {
+                //Hardcode code to be refactor in letter version
+
+                if (textBoxWhenEdit.Text.Length > 0 && textBoxWhenEdit.Text.Equals("Mission to be set") == false)
+                {
+                    labelShow.Content = textBoxWhenEdit.Text;
+                    if (!working)
+                    {
+                        DoWork();
+                    }
+                }
+
+            }
+        }
+        private void MenuEditClick(object sender, RoutedEventArgs e)
+        {
+            if (editing == false)
+            {
+                Toggle();
+            }
+        }
+
+        private void WhenDoubleClickOnItem(object sender, MouseButtonEventArgs e)
+        {
+            Toggle();
+        }
+
+        private void textBoxWhenEdit_LostFocus(object sender, RoutedEventArgs e)
+        {
+            Toggle();
+        }
+
+        private void TextBoxWhenEidt_PressEnter(object sender, KeyEventArgs e)
+        {
+            if (e.Key==Key.Enter)
+            {
+                Toggle();
+            }
+        }
+
+        private void CommitWork_Click(object sender, RoutedEventArgs e)
+        {
+            FinishWork();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
     }
 }
--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # CurrentMission
-A mission hints software simply hint what you should do
+
+A very very simple mission hints software simply hint what you should do
 
 for some bad memory guys who are often lose their concentration or messed thing up when they are coding or writing something . It's very tricky and useful to set a hint word directly tell what you should be doing right now , probably.
 
 just type what you are going to implement ,so that it will  give you get back to the situation letter.
+
+
+# Usage
+
+double click to edit your work,or right click to call up menu and select the *edit* option
+
+click the "√" button to clear you work.
+
+A letter version may record the work you have done.I am working on it(maybe)
+
